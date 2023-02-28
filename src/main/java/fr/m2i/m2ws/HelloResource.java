940b2ec00b6fce6d989cf2235e42e1cc5ba517f9
iTaghoury <idriss.taghoury@gmail.com>
2023-02-28 16:42:33 +0100

    Added Calcul functionalities

---
+++ src/main/java/fr/m2i/m2ws/HelloResource.java
@@ -0,0 +1,37 @@
+package fr.m2i.m2ws;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+import java.sql.SQLException;
+
+@Path("/calcul")
+public class HelloResource {
+    @GET
+    @Produces("text/html")
+    public String hello() {
+        return "<h1>Hello, World!</h1>";
+    }
+
+    @GET
+    @Path("/add")
+    @Produces("text/html")
+    public String add() {
+        return String.format("<h1>1 + 1 = %d</h1>", 1+1);
+    }
+
+    @GET
+    @Path("/sous")
+    @Produces("text/html")
+    public String sous() {
+        return String.format("<h1>3 - 2 = %d", 3-2);
+    }
+
+    @GET
+    @Path("/mult")
+    @Produces("text/html")
+    public String mult() {
+        return String.format("<h1>4 * 2 = %d", 4*2);
+    }
+}
