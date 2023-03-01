51a8153996b60b641277f7140592349f6fe15a45
iTaghoury <idriss.taghoury@gmail.com>
2023-03-01 11:58:09 +0100

    TP calculatrice changed endpoints

--- src/main/java/fr/m2i/m2ws/api/CalculResource.java
+++ src/main/java/fr/m2i/m2ws/api/CalculResource.java
@@ -8,28 +8,28 @@ import jakarta.ws.rs.PathParam;
 @Path("/calculatrice")
 public class CalculResource {
     @GET
-    @Path("/add/{n1}-{n2}")
+    @Path("/add/n1={n1}-n2={n2}")
     public String add(@PathParam("n1") int n1, @PathParam("n2") int n2) {
         return String.format("%d + %d = %d", n1, n2, Math.add(n1, n2));
     }
     @GET
-    @Path("/sub/{n1}-{n2}")
+    @Path("/sub/n1={n1}-n2={n2}")
     public String sub(@PathParam("n1") int n1, @PathParam("n2") int n2) {
         return String.format("%d - %d = %d", n1, n2, Math.sub(n1, n2));
     }
     @GET
-    @Path("/mult/{n1}-{n2}")
+    @Path("/mult/n1={n1}-n2={n2}")
     public String mult(@PathParam("n1") int n1, @PathParam("n2") int n2) {
         return String.format("%d * %d = %d", n1, n2, Math.mult(n1, n2));
     }
     @GET
-    @Path("/div/{n1}-{n2}")
+    @Path("/div/n1={n1}-n2={n2}")
     public String div(@PathParam("n1") int n1, @PathParam("n2") int n2) {
         return String.format("%d / %d = %f", n1, n2, Math.div(n1, n2));
     }
     @GET
-    @Path("/fact/{n1}")
-    public String fact(@PathParam("n1") int n1) {
-        return String.format("%d! = %d", n1, Math.fact(n1));
+    @Path("/fact/n={n}")
+    public String fact(@PathParam("n") int n) {
+        return String.format("%d! = %d", n, Math.fact(n));
     }
 }
